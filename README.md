d237fcfaef9e05d779d4b71fc0db98f2172132bf
kamshir <iammaksim2000shirshikov@gmail.com>
2022-01-22 12:18:11 +0300

    tabs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # iPhone 13 Pro - Promo Page
+
+![Image alt](https://github.com/kamshir/iPhone-13-Pro---Promo/raw/main/img/iPhone-graphite.webp)
--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,42 +1,35 @@
 const scroll = () => {
-  const links = document.querySelectorAll('.header-menu__item a')
-  const allCharater = document.querySelector('.card-details__link-characteristics')
-
-  seamless.polyfill()
-
-  links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-      event.preventDefault()
-      
-      const id = element.getAttribute('href')
-      const section = document.getElementById(id)
-
-      if (section) {
-        seamless.elementScrollIntoView(section, {
-          behavior: 'smooth',
-          block: 'start'
-        })
-      } else {
-        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-          behavior: 'smooth',
-          block: 'center',
-          inline: 'center'
-        })
-      }
-    })
-  })
-
-  allCharater.addEventListener('click', (event) => {
+  const links = document.querySelectorAll(".header-menu__item a");
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics');
+  
+  const newArray = [...links, linkCharacteristics];
+  
+  seamless.polyfill();
+  
+  newArray.forEach((element, index, array) => {
+    element.addEventListener("click", (event) => {
       event.preventDefault();
-
-      const allCharaterId = allCharater.getAttribute('href').substring(1);
-      const allCharaterSection = document.getElementById(allCharaterId);
-
-      seamless.elementScrollIntoView(allCharaterSection, {
+  
+      const id = element.getAttribute("href").substring(1);
+      const section = document.getElementById(id);
+  
+      if (section) {
+        seamless.scrollIntoView(section, {
           behavior: "smooth",
-          block: "start"
-      })
-  })
+          block: "start",
+        });
+      } else {
+        seamless.elementScrollIntoView(
+          document.querySelector("#characteristics"),
+          {
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+          }
+        );
+      }
+    });
+  });
 }
 
-scroll()
+scroll();
